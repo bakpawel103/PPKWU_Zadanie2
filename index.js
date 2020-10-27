82afa3c35bbb7f7d26b68df962c333b85b49489c
pawelbak <215675@edu.p.lodz.pl>
2020-10-27 14:37:00 +0100

    Imported to server file body-parser module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
   res.send('Welcome at site, that will check your password validity');
