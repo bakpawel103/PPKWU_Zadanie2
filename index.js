a77fbbe604443568437b4eee7e87263e0d24a0bb
pawelbak <215675@edu.p.lodz.pl>
2020-10-27 14:32:52 +0100

    Added listener which will listen at localhost address at port 3000.

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Welcome at site, that will check your password validity');
 });
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
