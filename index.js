69badf917d24086a8494623858677d0f0c732022
pawelbak <215675@edu.p.lodz.pl>
2020-10-27 14:38:04 +0100

    Imported to server file cors module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
